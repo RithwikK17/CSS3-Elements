5725be344b02f47a698b0487357cc26010d57257
RithwikK17 <rithwik2603@gmail.com>
2025-04-01 10:46:40 +0530

    protfolio final

--- 8.Project-1/JS/app.js
+++ 8.Project-1/JS/app.js
@@ -4,3 +4,7 @@ let menuItems = document.querySelector(".menu-items")
 togglerBtn.addEventListener("click", function(){
     menuItems.classList.toggle("active-menu")
 })
+
+//get current year from date object
+let yr=document.getElementById('yr')
+yr.innerText = new Date().getFullYear();
